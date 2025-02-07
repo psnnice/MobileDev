e685189482026ce5f0829f68584fe2f068016080
Yuu._. <TPxLolipopZ@gmail.com>
2025-02-08 04:56:11 +0700

    add_Profile

--- lib/frontend/page/providers/user_provider.dart
+++ lib/frontend/page/providers/user_provider.dart
@@ -4,10 +4,12 @@ class UserProvider with ChangeNotifier {
   String _role = '';
   String _username = '';
   int? _id = null;
+  String? _profileImagePath; // เพิ่มตัวแปรสำหรับเก็บ path ของรูปโปรไฟล์
 
   String get username => _username;
   int? get id => _id;
   String get role => _role;
+  String? get profileImagePath => _profileImagePath; // เพิ่ม getter สำหรับ profileImagePath
 
   void setRole(String role) {
     _role = role;
@@ -20,9 +22,15 @@ class UserProvider with ChangeNotifier {
     notifyListeners(); // แจ้งเตือนให้ UI อัปเดต
   }
 
+  void setProfileImagePath(String path) {
+    _profileImagePath = path;
+    notifyListeners(); // แจ้งเตือนให้ UI อัปเดต
+  }
+
   void clearUser() {
     _username = '';
     _id = null;
+    _profileImagePath = null; // ล้างข้อมูล profileImagePath ด้วย
     notifyListeners();
   }
 }
