6229ffcd2dbbaea630a162c0a303b37fa29c2f0c
nice <niceosurn@gmail.com>
2025-02-08 17:18:58 +0700

    built aad apk

--- lib/frontend/page/configip/config.dart
+++ lib/frontend/page/configip/config.dart
@@ -1,5 +1,5 @@
 class Config {
-  static const String ip = "11.0.100.209"; // Update the IP address here
+  static const String ip = "192.168.43.89"; // Update the IP address here
 }
 
 //yuu = 192.168.1.240
